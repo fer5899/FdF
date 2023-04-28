ca00353d9f46daab6acc07efdc18994b35b0eab5
fgomez-d <fgomez-d@student.42malaga.com>
2023-04-28 14:57:44 +0200

    Improve initialization of fdf

--- src/fdf_utils.c
+++ src/fdf_utils.c
@@ -6,7 +6,7 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 13:20:25 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/04/28 14:26:16 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/04/28 14:56:33 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,11 @@ void	check_args(int argc, char **argv)
 
 void	fdf_init(t_fdf *fdf, char **argv)
 {
+	ft_memset(fdf, 0, sizeof(t_fdf));
 	fdf->zoom = 1;
 	fdf->h_mod = STD_HMOD;
 	fdf->brush = 1;
-	fdf->color = 0;
 	fdf->persp = 'I';
-	fdf->mov_x = 0;
-	fdf->mov_y = 0;
 	parse_map(argv[1], fdf);
 	if (!fdf->map)
 	{
